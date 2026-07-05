3e4fb4cb68d4c3d4e1d17d1ffbe9bb288235e26b
youngbokhwang7-gif <youngbokhwang7@gmail.com>
2026-07-06 08:48:50 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'weave-v1';
+const ASSETS = ['./','index.html','manifest.json',
+  'icons/icon-192.png','icons/icon-512.png','icons/icon-512-maskable.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r => r ||
+      fetch(e.request).then(resp => {
+        const cp = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, cp));
+        return resp;
+      }).catch(() => caches.match('index.html'))
+    )
+  );
+});
